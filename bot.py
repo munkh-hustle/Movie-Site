cf5ce257af1179d236e2235059e524d352ce40ca
“Munkh-Erdene” <munkh.hustle@gmail.com>
2025-04-17 12:52:56 +0800

    Edit

--- bot.py
+++ bot.py
@@ -2117,7 +2117,7 @@ def main() -> None:
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(MessageHandler(filters.VIDEO, handle_video))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    application.add_handler(MessageHandler(filters.PHOTO & filters.CaptionRegex("payment|төлбөр"), handle_screenshot))
+    application.add_handler(MessageHandler(filters.PHOTO & filters.COMMAND, handle_screenshot))
     application.add_handler(MessageHandler(filters.PHOTO & ~filters.COMMAND, handle_photo))
 
     application.add_error_handler(error_handler)
